1e51c886d11133ba9e4767e641345c559179930c
Simone Gallo <simone@Guest.local>
2026-02-02 16:53:03 +0100

    fix icon resolutions

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow() {
     height: 900,
     minWidth: 800,
     minHeight: 600,
-    icon: path.join(__dirname, 'src/logo/icon.png'),
+    icon: path.join(__dirname, 'src/logo/icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
